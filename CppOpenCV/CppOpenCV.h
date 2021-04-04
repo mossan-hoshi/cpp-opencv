30dcdeeaa9712c35e13295f92d047d875c22d130
mossan-hoshi <sackn5@gmail.com>
2021-04-04 21:16:59 +0900

    change sample code

--- CppOpenCV/CppOpenCV.h
+++ CppOpenCV/CppOpenCV.h
@@ -1,15 +1,14 @@
 #pragma once
 
-#include "ArgParse.h"
 #include <opencv2/opencv.hpp>
+
+#include "ArgParse.h"
 
 void ImageProcess(ArgumentParser& args) {
   // You can now use OpenCV!!
   //// Followings are sample code(you can delete it).
-  cv::Mat image = cv::Mat::zeros(100, 100, CV_8UC3);
-  cv::line(image, cv::Point2d(0, 50), cv::Point2d(99, 50),
-           cv::Scalar(255, 0, 128), 5);
-  cv::imshow("result(image is also can watch by Image Watch(VS Extension))",
-             image);
-  cv::waitKey(0);
+  cv::Mat image = cv::imread(args["in_file_path"].get()[0]);
+  cv::line(image, cv::Point2d(629, 614), cv::Point2d(920, 445),
+           cv::Scalar(255, 0, 128), 50);
+  cv::imwrite(args["out_dir_path"].get()[0] + R"(\out.jpg)", image);
 }
